5325ccc034cfd1b7b836ec4247086a4825f9c714
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2017-08-26 14:08:02 -0300

    agrego comportamiento de Equals

--- Billing.Business/Models/Address.cs
+++ Billing.Business/Models/Address.cs
@@ -7,5 +7,24 @@ namespace Billing.Business.Models
         public string Province { get; set; }
 
         public string Country { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (Address)obj;
+
+            return this.Locality == other.Locality
+                && this.Province == other.Province
+                && this.Country == other.Country;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
